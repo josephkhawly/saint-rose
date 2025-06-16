47d86304b364db437e812a07b180e1ea3948215b
Joseph Khawly <joseph.khawly@gmail.com>
2025-06-16 14:11:53 -0600

    refactor: Staff popup consolidation

--- src/components/StaffMemberSpotlight.js
+++ src/components/StaffMemberSpotlight.js
@@ -1,43 +1,36 @@
-import React from "react";
+import React from 'react'
 
 import SlideAndFade from './SlideAndFade'
 
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-import MediaQuery from "react-responsive";
-
-import { MOBILEBP, DESKTOPTRANSITIONBP } from "../constants";
-
-function Video(link, closeHandler) {
+function Video({ link, closeHandler }) {
   return (
-    <div className="video-container">
-      <div className="inner-container">
-        <button className="close" onClick={() => closeHandler()}>
-          <img src="/images/close.svg" />
+    <div className='video-container'>
+      <div className='inner-container'>
+        <button className='close' onClick={() => closeHandler()}>
+          <img src='/images/close.svg' />
         </button>
         <video autoPlay controls>
-          <source src={link} type="video/mp4" />
+          <source src={link} type='video/mp4' />
         </video>
       </div>
     </div>
-  );
+  )
 }
 
-function Bio(name, role, photoLarge, bio, closeHandler) {
+function Bio({ name, role, photoLarge, bio, closeHandler }) {
   return (
-    <div className="bio-container">
-      <div className="photo-container">
-        <div
-          className="photo"
-          style={{ backgroundImage: `url(${photoLarge})` }}
-        />
+    <div className='bio-container'>
+      <div className='photo-container'>
+        <div className='photo' style={{ backgroundImage: `url(${photoLarge})` }} />
       </div>
-      <div className="inner-container">
-        <div className="text-container">
-          <SlideAndFade delay={350} distance="150px">
-            <div className="text">
-              <button className="close" onClick={() => closeHandler()}>
-                <img src="/images/close.svg" />
+      <div className='inner-container'>
+        <div className='text-container'>
+          <SlideAndFade delay={350} distance='150px'>
+            <div className='text'>
+              <button className='close' onClick={() => closeHandler()}>
+                <img src='/images/close.svg' />
               </button>
               <h3>{name}</h3>
               <h5>{role}</h5>
@@ -47,45 +40,26 @@ function Bio(name, role, photoLarge, bio, closeHandler) {
         </div>
       </div>
     </div>
-  );
-}
-
-function MobileBio(name, role, photoLarge, bio, closeHandler) {
-  return (
-    <div className="mobile-bio-container">
-      <div className="text-container">
-        <SlideAndFade delay={350} distance="150px">
-          <div className="text">
-            <button className="close" onClick={() => closeHandler()}>
-              <img src="/images/close.svg" />
-            </button>
-            <h3>{name}</h3>
-            <h5>{role}</h5>
-            {documentToReactComponents(bio)}
-          </div>
-        </SlideAndFade>
-      </div>
-    </div>
-  );
+  )
 }
 
 function StaffMemberSpotlight({ staffMemberDetails, closeHandler }) {
-  const { name, role, photoLarge, bio, video } = staffMemberDetails;
+  const { name, role, photoLarge, bio, video } = staffMemberDetails
   return (
-    <div className="staff-member-spotlight">
-      <MediaQuery minWidth={DESKTOPTRANSITIONBP}>
-          {video
-            ? Video(video, closeHandler)
-            : Bio(name, role, photoLarge, bio, closeHandler)}
-      </MediaQuery>
-
-      <MediaQuery maxWidth={MOBILEBP}>
-          {video
-            ? Video(video, closeHandler)
-            : MobileBio(name, role, photoLarge, bio, closeHandler)}
-      </MediaQuery>
+    <div className='staff-member-spotlight'>
+      {video ? (
+        <Video link={video} closeHandler={closeHandler} />
+      ) : (
+        <Bio
+          name={name}
+          role={role}
+          photoLarge={photoLarge}
+          bio={bio}
+          closeHandler={closeHandler}
+        />
+      )}
     </div>
-  );
+  )
 }
 
-export default StaffMemberSpotlight;
+export default StaffMemberSpotlight
