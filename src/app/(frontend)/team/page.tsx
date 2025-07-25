1215b5002423d6b6815db9b264779e70e39457d6
Joseph Khawly <joseph.khawly@gmail.com>
2025-07-25 14:27:46 -0600

    refactor: use intro component on team page, remove redundant styles

--- src/app/(frontend)/team/page.tsx
+++ src/app/(frontend)/team/page.tsx
@@ -1,3 +1,4 @@
+import { IntroText } from '@/components/IntroText'
 import SlideAndFade from '@/components/SlideAndFade'
 import { StaffMemberGrid } from '@/components/StaffMember'
 import { getStaff } from '@/lib/helpers'
@@ -14,17 +15,10 @@ export default async function Team() {
       <div className='content-container'>
         <div className='content'>
           <div className='inner-content-container'>
-            <div className='header'>
-              <SlideAndFade delay={1.25}>
-                <h5>Meet the team</h5>
-              </SlideAndFade>
-              <SlideAndFade delay={1.45}>
-                <h3>
-                  Not-your-ordinary-hairdressers. Meet the people behind the chair. See someone you
-                  vibe with? Let us know when you book and we&apos;ll make the match.
-                </h3>
-              </SlideAndFade>
-            </div>
+            <IntroText
+              title='Meet the team'
+              introText="Not-your-ordinary-hairdressers. Meet the people behind the chair. See someone you vibe with? Let us know when you book and we'll make the match."
+            />
 
             <SlideAndFade delay={1.65}>
               <StaffMemberGrid staffMembers={staffMembers} />
