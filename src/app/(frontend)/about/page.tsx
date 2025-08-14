0fdb9ae9d5c084fb61c35c7a816ac520a252d556
Joseph Khawly <joseph.khawly@gmail.com>
2025-08-14 11:55:51 -0600

    refactor(about): move Instagram block to its own component

--- src/app/(frontend)/about/page.tsx
+++ src/app/(frontend)/about/page.tsx
@@ -1,4 +1,3 @@
-import Iframe from 'react-iframe'
 import { Metadata } from 'next'
 import { queryPageBySlug } from '@/lib/helpers'
 import { RenderBlocks } from '@/blocks/RenderBlocks'
@@ -10,27 +9,8 @@ export const metadata: Metadata = {
 export default async function About() {
   const page = await queryPageBySlug({ slug: 'about' })
   return (
-    <div className='about'>
-      <div className='content-container'>
-        <div className='content'>
-          <RenderBlocks blocks={page?.layout || []} />
-
-          <div className='our-clients'>
-            <div className='instagram-reviews'>
-              <Iframe
-                url='https://cdn.lightwidget.com/widgets/d9467bad991b50808baea81bd806ab73.html'
-                width='100%'
-                height='auto'
-                scrolling='no'
-                display='initial'
-                position='relative'
-                // allowtransparency='true'
-                // style='width:100%;border:0;overflow:hidden;'
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className='content'>
+      <RenderBlocks blocks={page?.layout || []} />
     </div>
   )
 }
