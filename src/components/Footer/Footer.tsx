7766ed120b14136379471e06964305f0e8b5f256
Joseph Khawly <joseph.khawly@gmail.com>
2026-03-25 23:04:55 -0600

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,73 +1,76 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import { Fade } from 'react-awesome-reveal'
-import TransitionLink from '../TransitionLink'
+import { toRoman } from '@/lib/toRoman'
 
 function Footer() {
   return (
-    <Fade fraction={0.01} triggerOnce>
-      <footer className='text-secondary bg-black'>
-        <Fade triggerOnce>
-          <div className='flex flex-col items-center justify-center p-[84px]'>
-            <div>
-              <Image
-                src='/images/footer-rose.svg'
-                alt='Footer Rose'
-                width={124}
-                height={142}
-                className='max-sm:w-[82px]'
-              />
+    <footer className='pt-12 pb-5 lg:pt-11 lg:pb-[26px] w-full px-8'>
+      <div className='flex flex-col md:flex-row md:gap-12 justify-between mb-12 md:mb-0'>
+        <div className='text-sm'>
+          <div className='flex flex-col gap-8'>
+            <div className=''>
+              <p className='text-sm'>3316 Mount Vernon, Houston</p>
+              <p className='text-sm'>
+                <Link href='tel:3468022183'>346 802 2183</Link>
+              </p>
+              <Link href='mailto:info@hairbysaintrose.com' className='lg:mb-[22px]'>
+                info@hairbysaintrose.com
+              </Link>
+              <p className='text-sm'></p>
             </div>
-
-            <div className='mt-12 flex w-full flex-col items-center justify-center gap-6 lg:flex-row'>
-              <TransitionLink
-                href='/careers'
-                className='font-ap-bold text-secondary text-[15px] uppercase no-underline'
-              >
-                careers
-              </TransitionLink>
+            <div className='mb-9 flex gap-4'>
               <Link
                 href='https://hairbysaintrose.direct.salonservicegroup.com'
                 target='_blank'
-                className='font-ap-bold text-secondary text-[15px] uppercase no-underline'
+                className='text-sm hover:text-rose transition-colors duration-300'
               >
-                shop
+                Shop
               </Link>
-            </div>
-
-            <div className='mt-12 flex flex-col items-center justify-center gap-8 lg:flex-row'>
-              <p className='mt-[22px] lg:mt-0'>
-                &copy; {new Date().getFullYear()} Hair by Saint Rose
-              </p>
-              <div className='order-first flex items-center gap-x-5 lg:order-none'>
-                <Link
-                  href='https://www.facebook.com/hairbysaintrose/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <Image src='/images/facebook.svg' alt='Facebook' width={35} height={35} unoptimized />
+              {/* <p className='text-sm'>
+                <Link href='/careers' className='hover:text-rose transition-colors duration-300'>
+                  Careers
                 </Link>
-                <Link
-                  href='https://www.instagram.com/hairbysaintrose'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <Image src='/images/instagram.svg' alt='Instagram' width={35} height={35} unoptimized />
-                </Link>
-                <Link
-                  href='https://www.youtube.com/channel/UC-m8XjZ7A9vwDpch45bJbtA'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <Image src='/images/youtube.svg' alt='YouTube' width={35} height={35} unoptimized />
-                </Link>
-              </div>
-              <p>website by <Link href='https://josephkhawly.dev' target='_blank' rel='noreferrer' className='text-secondary no-underline'>Joseph Khawly</Link></p>
+              </p> */}
             </div>
           </div>
-        </Fade>
-      </footer>
-    </Fade>
+        </div>
+        <div className='flex flex-row md:flex-col gap-4 justify-start'>
+          <Link
+            href='https://www.instagram.com/hairbysaintrose/'
+            target='_blank'
+            rel='noreferrer'
+            className='text-sm hover:text-rose transition-colors duration-300'
+          >
+            Instagram
+          </Link>
+          <Link
+            href='https://www.facebook.com/hairbysaintrose/'
+            target='_blank'
+            rel='noreferrer'
+            className='text-sm hover:text-rose transition-colors duration-300'
+          >
+            Facebook
+          </Link>
+          <Link
+            href='https://www.youtube.com/channel/UC-m8XjZ7A9vwDpch45bJbtA'
+            target='_blank'
+            rel='noreferrer'
+            className='text-sm hover:text-rose transition-colors duration-300'
+          >
+            YouTube
+          </Link>
+        </div>
+      </div>
+      <div className='text-xs'>
+        <span className='mb-[9px] lg:mb-0 lg:mr-10 block lg:inline-block'>
+          © {toRoman(new Date().getFullYear())} Saint Rose
+        </span>
+        <span>Site design by Joseph Khawly</span>
+      </div>
+      {/* Big Logo */}
+      {/* <h1 className='font-fautive text-7xl md:text-8xl lg:text-[19rem] text-black text-center'>
+        SAINT ROSE
+      </h1> */}
+    </footer>
   )
 }
 
