8cbdb9863e09a065872fee04ee6a08a4c2c94e63
Joseph Khawly <joseph.khawly@gmail.com>
2026-04-01 11:35:14 -0600

    move richtext styles to main css file

--- src/blocks/RichText/Component.tsx
+++ src/blocks/RichText/Component.tsx
@@ -1,7 +1,6 @@
 import { RichText } from '@payloadcms/richtext-lexical/react'
 import type { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
-import styles from './richtext.module.css'
 
 export const RichTextComponent = ({ content }: { content: SerializedEditorState }) => {
-  return <RichText data={content} className={styles['payload-richtext']} />
+  return <RichText data={content} className={'payload-richtext'} />
 }
