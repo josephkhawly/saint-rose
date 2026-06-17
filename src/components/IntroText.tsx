8691c3756f179502454b506e17b16a46294fd64a
Joseph Khawly <joseph.khawly@gmail.com>
2026-06-17 15:57:12 -0600

    feat: add image next to intro text

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -1,6 +1,8 @@
-export const IntroText = ({ title, introText }: { title: string; introText: string }) => {
+import Image, { StaticImageData } from "next/image";
+
+export const IntroText = ({ title, introText, image }: { title: string; introText: string; image?: StaticImageData | string }) => {
   return (
-    <section className='w-full min-h-screen px-6 py-32 md:py-40 lg:py-48'>
+    <section className='w-full px-6 py-32 md:py-40 lg:py-48'>
       <div className="relative flex flex-col md:flex-row items-end gap-12">
         <div className="w-full md:w-7/12">
           <h1 className="font-caslon text-xl uppercase">
@@ -10,6 +12,17 @@ export const IntroText = ({ title, introText }: { title: string; introText: stri
             {introText}
           </p>
         </div>
+        {image && (
+          <div className="w-full md:w-5/12 aspect-4/5 overflow-hidden">
+            <Image className="w-full h-full object-cover grayscale-[0.2] contrast-125"
+              alt="Close-up of a high-end salon interior with minimal stone textures, soft warm lighting, and a single designer chair in soft focus"
+              src={image}
+              placeholder='blur'
+              width={500}
+              height={500}
+            />
+          </div>
+        )}
       </div>
     </section>
   )
